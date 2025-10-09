a2c9ee7ad043a3a63831d02fac8fcfbd284ece4c
Hau99082005 <hau99082005@gmail.com>
2025-10-09 22:09:06 +0700

    main

--- src/main/java/com/shop/developer/controllers/BaseController.java
+++ src/main/java/com/shop/developer/controllers/BaseController.java
@@ -1,5 +1,27 @@
 package com.shop.developer.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class BaseController {
+    //tạo phương thức trả về(response) chuẩn cho tất cả API
+    protected ResponseEntity<Map<String, Object>> successResponse(Object data) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", true);
+        response.put("data", data);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+     protected ResponseEntity<Map<String, Object>> errorResponse(String message, HttpStatus status) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", false);
+        response.put("error", message);
+        return new ResponseEntity<>(response, status);
+    }
+     protected void logAction(String message) {
+        System.out.println("[BaseController] " + message);
+    }
     
 }
