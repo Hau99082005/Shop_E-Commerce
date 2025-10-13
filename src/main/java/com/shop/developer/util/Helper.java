b1c42e77bccaa763c619351bd0e6018052897a26
Hau99082005 <hau99082005@gmail.com>
2025-10-13 09:20:59 +0700

    main

--- src/main/java/com/shop/developer/util/Helper.java
+++ src/main/java/com/shop/developer/util/Helper.java
@@ -1,5 +1,18 @@
 package com.shop.developer.util;
 
+import java.util.List;
+
+import com.shop.developer.modules.OrderItems.models.OrderItems;
+
 public class Helper {
-    
+    public static double total(List<OrderItems> orderItemsList) {
+        if (orderItemsList == null || orderItemsList.isEmpty()) return 0.0;
+        double total = 0.0;
+        for (OrderItems orderItemsList1 : orderItemsList) {
+            if(orderItemsList1 == null) continue;
+            OrderItems orderItems = orderItemsList.get(0);
+            total += orderItems.getPrice() * orderItems.getQuantity();
+        }
+        return total;
+    }
 }
